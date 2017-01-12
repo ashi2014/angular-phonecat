1295b941a45b61aa776a3bad7b915249e7df4db6
Angela Shi <Angela Shi>
2017-01-12 11:19:13 -0800

    step-3

--- app/app.js
+++ app/app.js
@@ -1,16 +1,1 @@
-var phoneCatApp = angular.module("phoneCatApp", []);
-
-phoneCatApp.controller("PhoneListController", function($scope) {
-	$scope.phones = [
-    {
-      name: 'Nexus S',
-      snippet: 'Fast just got faster with Nexus S.'
-    }, {
-      name: 'Motorola XOOM™ with Wi-Fi',
-      snippet: 'The Next, Next Generation tablet.'
-    }, {
-      name: 'MOTOROLA XOOM™',
-      snippet: 'The Next, Next Generation tablet.'
-    }
-  ];
-});
+angular.module("phonecatApp", []);
