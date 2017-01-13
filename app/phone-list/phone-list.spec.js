9c683d6c3be82ae150cf91c30951a813afcd42ea
Angela Shi <Angela Shi>
2017-01-13 10:24:49 -0800

    step-7

--- app/phone-list/phone-list.spec.js
+++ app/phone-list/phone-list.spec.js
@@ -5,14 +5,22 @@ describe('phoneList', function() {
 
   // Test the controller
   describe('PhoneListController', function() {
-  	var ctrl;
+  	var $httpBackend, ctrl;
 
-  	beforeEach(inject(function($componentController) {
+  	beforeEach(inject(function($componentController, _$httpBackend_) {
   		ctrl = $componentController("phoneList");
+
+  		$httpBackend = _$httpBackend_;
+  		$httpBackend.expectGET("phones/phones.json")
+  					.respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
   	}));
 
-    it('should create a `phones` model with 3 phones', function() {
-      expect(ctrl.phones.length).toBe(3);
+    it('should create a `phones` model with 2 phones from $http', function() {
+      expect(ctrl.phones).toBeUndefined();
+
+      $httpBackend.flush();
+
+      expect(ctrl.phones.length).toBe(2);
     });
 
     it('should set the default order to age', function() {
